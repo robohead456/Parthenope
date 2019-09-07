90f2f611cbf11b504422c214507cee26c05ecad9
robohead456 <m.sidler456@gmail.com>
2019-09-07 10:47:04 -0400

    new main file allows for easier changing of hole numbers as well as more than 2 hole sets

--- software/nucleo-pio/src/main.cpp
+++ software/nucleo-pio/src/main.cpp
@@ -8,50 +8,87 @@
 /* ---------------------------------------------------------------------------------------- */
 
 #define VERBOSE 				(1)
-#define DISK_AXIS 				(1)
+
 #define DISK_MOTOR_POLE_PAIRS 	(2)
-#define LOW_DISK_HOLES 			(2)
-#define HIGH_DISK_HOLES 		(2*LOW_DISK_HOLES)
-#define HIGH_HOLE 				(1)
-#define LOW_HOLE 				(0)
+#define DISK_AXIS				(1)
+
 #define PI           			(3.14159265358979323846)
+
 #define STATE_DISABLED			(2)
 #define STATE_PLAYING 			(1)
 #define STATE_NOT_PLAYING 		(0)
 
+#define MODE_MONOPHONIC_REGULAR (0)
+#define MODE_POLYPHONIC_REGULAR (1)
+#define MODE_MONOPHONIC_LEGATO  (2)
+#define MODE_POLYPHONIC_LEGATO	(3)
+#define MODE_DIRECT_CONTROL		(4)
+
+#define VOLUME1					(1)
+#define VOLUME2 				(2)
+#define VOLUME3 				(3)
+
+#define DEFAULT_NOTE			(45)
+#define DEFAULT_HOLE			(0)
+#define DEFAULT_STATE			STATE_NOT_PLAYING
+#define DEFAULT_VOLUME			VOLUME3
+
 /* ---------------------------------------------------------------------------------------- */
 
-const char* instrumentName = "OSC-Test";
+const char* instrumentName = "Parthenope";
 
-//Setup digital outputs
 DigitalOut led1(LED1);	//Green running LED
 DigitalOut led2(LED2);	//Blue controller LED
 DigitalOut led3(LED3);	//Red ethernet LED
 
-DigitalOut lowHoleSolenoid(SOLENOID_A01);
-DigitalOut highHoleSolenoid(SOLENOID_A02);
+uint8_t numHoleSets = 4;
+uint8_t numHoles[] = {4, 6, 8, 10};
+
+DigitalOut solenoids_vol3[] =  {SOLENOID_A03,
+						  		SOLENOID_A04,
+						  		SOLENOID_A01,
+						  		SOLENOID_A02};
+DigitalOut solenoids_vol2[] =  {SOLENOID_A08,
+						  		SOLENOID_A09,
+						  		SOLENOID_A06,
+						  		SOLENOID_A07};
+DigitalOut solenoids_vol1[] =  {SOLENOID_A13,
+						  		SOLENOID_A14,
+						  		SOLENOID_A11,
+						  		SOLENOID_A12};						  								  								  																  
+
+uint8_t currentNote = DEFAULT_NOTE;
+uint8_t currentHole = DEFAULT_HOLE;
+uint8_t currentState = DEFAULT_STATE;
+uint8_t currentVolume = DEFAULT_VOLUME;
 
 /* ---------------------------------------------------------------------------------------- */
 
-void setSpeed(ODriveMbed odrv, uint8_t midiNote, uint8_t hole) {
+uint8_t mapVelocity(uint8_t x) {
+	return ((x * 5 / 127) + 1);
+}
+
+float getSpeed(uint8_t midiNote, uint8_t holeSet) {
 	
-	float speed = 0;
-	if(hole == LOW_HOLE) {
-		speed = ((440.0 * pow(pow(2,1.0/12.0), (midiNote-69)) / LOW_DISK_HOLES) * 60.0  * 2.0 * PI / 60.0 * DISK_MOTOR_POLE_PAIRS);
+	if(holeSet < numHoleSets) {
+		return ((440.0 * pow(pow(2,1.0/12.0), (midiNote-69)) / numHoles[holeSet]) * 60.0  * 2.0 * PI / 60.0 * DISK_MOTOR_POLE_PAIRS);
 	}
-	else if(hole == HIGH_HOLE) {
-		speed = ((440.0 * pow(pow(2,1.0/12.0), (midiNote-69)) / HIGH_DISK_HOLES) * 60.0  * 2.0 * PI / 60.0 * DISK_MOTOR_POLE_PAIRS);
+	else return -1;	
+}
+
+void setSpeed(ODriveMbed odrv, uint8_t midiNote, uint8_t holeSet) {
+	
+	float speed = getSpeed(midiNote, holeSet);
+	
+	if(speed > 0) {
+		printf("Setting Velocity\r\n");
+		odrv.setVelocity(DISK_AXIS, speed);
+		printf("Done\r\n");
 	}
-	else return;
-	
-	printf("Setting Velocity\r\n");
-	odrv.setVelocity(DISK_AXIS, speed);
-	printf("Done\r\n");
-	
 }
 
 void stopDisk(ODriveMbed odrv) {
-	setSpeed(odrv, 45, LOW_HOLE);
+	setSpeed(odrv, DEFAULT_NOTE, DEFAULT_HOLE);
 	wait(4);
 	odrv.run_state(DISK_AXIS, ODriveMbed::AXIS_STATE_IDLE, 0);
 }
@@ -60,23 +97,40 @@ void startDisk(ODriveMbed odrv) {
 	odrv.run_state(DISK_AXIS, ODriveMbed::AXIS_STATE_SENSORLESS_CONTROL, 0);
 	odrv.setControlMode(DISK_AXIS, ODriveMbed::CTRL_MODE_VELOCITY_CONTROL, 0);
 	wait(4);
-	setSpeed(odrv, 45, LOW_HOLE);
+	setSpeed(odrv, DEFAULT_NOTE, DEFAULT_HOLE);
 }
 
-void solenoidOn(uint8_t hole) {
-	if(hole == HIGH_HOLE) 
-		highHoleSolenoid = 1;
-	else if(hole == LOW_HOLE)
-		lowHoleSolenoid = 1;
-	return;	
+void solenoidOn(uint8_t holeSet, uint8_t volume) {
+	if(holeSet < numHoleSets) {
+		currentVolume = volume;
+		switch(volume) {
+			case VOLUME1:
+				solenoids_vol1[holeSet] = 1;
+				break;
+			case VOLUME2:
+				solenoids_vol2[holeSet] = 1;
+				break;
+			default:
+				solenoids_vol3[holeSet] = 1;
+				break;
+		}
+	} 
 }
 
-void solenoidOff(uint8_t hole) {
-	if(hole == HIGH_HOLE) 
-		highHoleSolenoid = 0;
-	else if(hole == LOW_HOLE)
-		lowHoleSolenoid = 0;
-	return;	
+void solenoidOff(uint8_t holeSet) {
+	if(holeSet < numHoleSets) {
+		switch(currentVolume) {
+			case VOLUME1:
+				solenoids_vol1[holeSet] = 0;
+				break;
+			case VOLUME2:
+				solenoids_vol2[holeSet] = 0;
+				break;
+			default:
+				solenoids_vol3[holeSet] = 0;
+				break;
+		}
+	} 
 }
 
 /* ---------------------------------------------------------------------------------------- */
@@ -100,7 +154,7 @@ int main() {
 	//Setup ethernet interface
 	EthernetInterface eth;
 	eth.connect();
-	if(VERBOSE) pc.printf("Client IP Address:     %s\r\n", eth.get_ip_address());
+	if(VERBOSE) pc.printf("Client IP Address:         %s\r\n", eth.get_ip_address());
 	led3 = 0;	//turn off red LED
 
 	//Setup OSC client
@@ -112,10 +166,6 @@ int main() {
 	//Create a pointer to an OSC Message
 	OSCMessage* msg = (OSCMessage*)malloc(sizeof(OSCMessage*));
 
-	uint8_t currentNote = 45;
-	uint8_t hole = LOW_HOLE;
-	uint8_t state = STATE_NOT_PLAYING;
-
 	wait(10); // to ensure that the motor is calibrated
 
 	startDisk(odrv);
@@ -126,7 +176,7 @@ int main() {
 		pc.printf("\r\nGot a message\r\n");
 		
 		//Check that the message is for this instrument
-		if(strcmp(msg->address, "/OSC-Test/play") == 0) {
+		if(strcmp(msg->address, "/Parthenope/play") == 0) {
 
 			pc.printf("For this instrument\r\n");
 
@@ -139,133 +189,79 @@ int main() {
 
 				//pc.printf("%p\r\n", (void*)msg);
 				//pc.printf("%d %d\r\n",pitch,velocity);
-				if(state != STATE_DISABLED) {
-						if(velocity > 0) {
-						pc.printf("note on message: %d\r\n",pitch);
-						if(pitch == currentNote) { // The disk is already at the required velocity
-							pc.printf("current note\r\n");
-							solenoidOn(hole);
-							currentNote = pitch;
-							state = STATE_PLAYING;
-						}
-						else if((hole == LOW_HOLE) && (pitch == currentNote+12)) { // The disk is at a correct velocity for the high hole
-							pc.printf("current speed but on high hole\r\n");
-							hole = HIGH_HOLE;
-							solenoidOn(hole);
-							currentNote = pitch;
-							state = STATE_PLAYING;
-						}
-						else if((hole == HIGH_HOLE) && (pitch == currentNote-12)) { // The disk is at a correct velocity for the high hole
-							pc.printf("current speed but on low hole\r\n");
-							hole = LOW_HOLE;
-							solenoidOn(hole);
-							currentNote = pitch;
-							state = STATE_PLAYING;
-						}
-						else if(state == STATE_NOT_PLAYING) { // The disk velocity needs to be changed to the correct velocity befor playing
-							if(hole == HIGH_HOLE) {
-								pc.printf("not playing - currently on high hole\r\n");
-								if(pitch > currentNote) {
-									pc.printf("high hole speed up\r\n");
-									setSpeed(odrv, pitch, hole);
-									pc.printf("Set speed\r\n");
-									//TODO delay?
-									solenoidOn(hole);
-									pc.printf("solenoid on\r\n");
-									currentNote = pitch;
-									state = STATE_PLAYING;
-									pc.printf("Set states\r\n");
-								}
-								else { // (pitch < currentNote)
-									if(currentNote-pitch < 6) {
-										pc.printf("high hole slow down\r\n");
-										setSpeed(odrv, pitch, hole);
-										//TODO delay? 
-										solenoidOn(hole);
-										currentNote = pitch;
-										state = STATE_PLAYING;
-									}
-									else { //(currentNote-pitch > 6)
-										pc.printf("low hole slow down\r\n");
-										setSpeed(odrv, pitch, hole);
-										//TODO delay?
-										hole = LOW_HOLE;
-										solenoidOn(hole);
-										currentNote = pitch;
-										state = STATE_PLAYING;
-									}
+
+				if(currentState != STATE_DISABLED) {
+					if(velocity > 0) { // note on message
+						if(currentState == STATE_NOT_PLAYING) { // not playing a note
+
+							// calculate which hole requires the least velocity change
+							float minVelChange = infinityf();
+							int8_t minVelHole = -1;
+							for(uint8_t i = 0; i < numHoleSets; i++) {
+								float diff = abs(getSpeed(pitch, i) - getSpeed(currentNote, currentHole));
+								pc.printf("Hole set %d, change %f\r\n", i, diff);
+								if( diff < minVelChange ) {
+									minVelHole = i;
+									minVelChange = diff;
+									pc.printf("Set to hole set %d",i);
 								}
 							}
-							else { // (hole == LOW_HOLE)
-								pc.printf("not playing - currently on low hole\r\n");
-								if(pitch < currentNote) {
-									pc.printf("low hole slow down\r\n");
-									setSpeed(odrv, pitch, hole);
-									//TODO delay?
-									solenoidOn(hole);
-									currentNote = pitch;
-									state = STATE_PLAYING;
-								}
-								else { // (pitch > currentNote)
-									if(pitch - currentNote < 6) {
-										pc.printf("low hole speed up\r\n");
-										setSpeed(odrv, pitch, hole);
-										//TODO delay?
-										solenoidOn(hole);
-										currentNote = pitch;
-										state = STATE_PLAYING;
-									}
-									else { // (pitch - currentNote > 6)
-										pc.printf("high hole speed up\r\n");
-										setSpeed(odrv, pitch, hole);
-										//TODO delay?
-										hole = HIGH_HOLE;
-										solenoidOn(hole);
-										currentNote = pitch;
-										state = STATE_PLAYING;
-									}
-								}
+
+							pc.printf("Best hole set %d", minVelHole);
+
+							// Set the speed and play the note
+							if(minVelHole != -1) {
+								currentNote = pitch;
+								currentHole = minVelHole;
+								currentState = STATE_PLAYING;
+
+								setSpeed(odrv, currentNote, currentHole);
+								solenoidOn(currentHole, mapVelocity(velocity));
 							}
+
 						}
-						else { // currently playing a note and should glissando up to it
+						else { //playing a note - glissando up/down
 							//TODO
 						}
 					}
-					else { // velocity == 0
+					else { // note off message
 						if(pitch == currentNote) {
-							pc.printf("note off message\r\n");
-							solenoidOff(hole);
-							state = STATE_NOT_PLAYING;
-						}
-						else { // got an off message for a different note and should ignore
+							currentState = STATE_NOT_PLAYING;
 
+							solenoidOff(currentHole);
+						}
+						else {
+							//do nothing
 						}
 					}
-				}
-				else { // state == STATE_DISABLED
-					// do nothing
-				}
+				}			
 			}
-		}
+		} // if(strcmp(msg->address, "/Parthenope/play") == 0)
 
 		// Turn off all notes
-		else if(strcmp(msg->address, "/OSC-Test/allNotesOff") == 0) {
-			if(state != STATE_DISABLED) {
+		else if(strcmp(msg->address, "/Parthenope/allNotesOff") == 0) {
+			if(currentState != STATE_DISABLED) {
 				pc.printf("All notes off\r\n");
-				solenoidOff(HIGH_HOLE);
-				solenoidOff(LOW_HOLE);
-				state = STATE_NOT_PLAYING;
+				for(uint8_t i = 0; i < numHoleSets; i++) {
+					solenoids_vol1[i] = 0;
+					solenoids_vol2[i] = 0;
+					solenoids_vol3[i] = 0;
+				}
+				currentState = STATE_NOT_PLAYING;
 			}
 		}
 
 		// Turn off disk
-		else if(strcmp(msg->address, "/OSC-Test/stopDisk") == 0) {
+		else if(strcmp(msg->address, "/Parthenope/stopDisk") == 0) {
 			pc.printf("Stop Disk\r\n");
-			if(state != STATE_DISABLED) {
-				state = STATE_DISABLED;
+			if(currentState != STATE_DISABLED) {
+				currentState = STATE_DISABLED;
+				for(uint8_t i = 0; i < numHoleSets; i++) {
+					solenoids_vol1[i] = 0;
+					solenoids_vol2[i] = 0;
+					solenoids_vol3[i] = 0;
+				}
 				stopDisk(odrv);
-				solenoidOff(HIGH_HOLE);
-				solenoidOff(LOW_HOLE);
 				pc.printf("Disk Stopped\r\n");
 			}
 			else {
@@ -273,14 +269,15 @@ int main() {
 			}
 		}
 
-		// Turn off disk
-		else if(strcmp(msg->address, "/OSC-Test/startDisk") == 0) {
+		// Turn on disk
+		else if(strcmp(msg->address, "/Parthenope/startDisk") == 0) {
 			pc.printf("Start Disk\r\n");
-			if(state == STATE_DISABLED) {
+			if(currentState == STATE_DISABLED) {
 				startDisk(odrv);
 				pc.printf("Disk Started\r\n");
-				state = STATE_NOT_PLAYING;
-				currentNote = 45;
+				currentState = STATE_NOT_PLAYING;
+				currentNote = DEFAULT_NOTE;
+				currentHole = DEFAULT_HOLE;
 			}
 			else {
 				pc.printf("Disk already started\r\n");
